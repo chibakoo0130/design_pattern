08dbf1a304bf59a3c2523afe466b21878382763c
Kotaro Chiba <kohtarocb@gmail.com>
2019-12-02 22:40:16 +0900

    ch07_Builder/sampleの追加

---
+++ ch07_Builder/sample/Builder.java
@@ -0,0 +1,6 @@
+public abstract class Builder {
+    public abstract void makeTitle(String title);
+    public abstract void makeString(String str);
+    public abstract void makeItems(String[] item);
+    public abstract void close();
+}
