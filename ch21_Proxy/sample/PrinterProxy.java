e87b2348222a157796f7fc0fa84e0bec81b326e8
Kotaro Chiba <kohtarocb@gmail.com>
2020-02-28 13:23:22 +0900

    ch21_Proxy/の追加

---
+++ ch21_Proxy/sample/PrinterProxy.java
@@ -0,0 +1,28 @@
+public class PrinterProxy implements Printable {
+    private String name;            // 名前
+    private Printer real;           // 「本人」
+    public PrinterProxy(String name) {  // コンストラクタ
+        this.name = name;
+    }
+    @Override
+    public synchronized void setPrinterName(String name) {  // 名前の設定
+        if (real != null) {
+            real.setPrinterName(name);  // 「本人」にも設定する
+        }
+        this.name = name;
+    }
+    @Override
+    public String getPrinterName() {    // 名前の取得
+        return name;
+    }
+    @Override
+    public void print(String string) {  // 表示
+        realize();
+        real.print(string);
+    }
+    private synchronized void realize() {   // 「本人」を生成
+        if (real == null) {
+            real = new Printer(name);
+        }
+    }
+}
